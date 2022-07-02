9b8d6064bd8cbce165bdd56238c2447b0697c4f9
akiraafu <junrongfu6@gmail.com>
2022-07-02 16:02:48 +0800

    section 4

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,24 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./expenseItem.css";
 
 export const ExpenseItem = (props) => {
-    //useState must be called directly inside the component funtion
-    const [title, setTitle] = useState(props.title);
+    // //useState must be called directly inside the component funtion
 
-    const clickHandler = () => {
-        setTitle("Updated!!!");
-        console.log(title);
-    };
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            {/* <button onClick={clickHandler}>Change Title</button> */}
         </Card>
     );
 };
